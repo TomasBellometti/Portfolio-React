6ea9a7277977e33bc1338ef3e63b8a11cf12195b
TomasBellometti <tomasbellometti@gmail.com>
2021-07-01 23:16:50 -0300

    cambio de header

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
     return (
         <div className="hero">
             <h1 className="hero-title">
-                Tomi's Blog
+                TOMI´S
             </h1>
-            <span className="hero-subtitle">Procura coquetearme más</span>
+            <span className="hero-subtitle">Diseño y desarrollo web</span>
             {/* <div className="hero-img"> */}
                 {/* <img src={Img} alt="" /> */}
             {/* </div> */}
