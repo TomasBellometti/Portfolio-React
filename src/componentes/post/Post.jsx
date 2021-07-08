2e0889b2ca190a02e5d0ea354858ae4b82013245
TomasBellometti <tomasbellometti@gmail.com>
2021-07-08 16:47:48 -0300

    agregado de Link al sitio

--- src/componentes/post/Post.jsx
+++ src/componentes/post/Post.jsx
@@ -7,9 +7,9 @@ function Post() {
         <img className="post-img" src="https://tomasbellometti.com.ar/img/projects/project-tattoo.jpg" alt="" />
         <div className="post-info">
             <div className="post-categorias">
+                <span className="post-cat">Adobe Xd</span>
+                <span className="post-cat">UI/UX</span>
                 <span className="post-cat">Diseño Web</span>
-                <span className="post-cat">PHP</span>
-                <span className="post-cat">JS</span>
             </div>
             <span className="post-title">
                 Lorem ipsum dolor sit amet.
@@ -18,6 +18,7 @@ function Post() {
         <p className="post-desc">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque dignissimos ad voluptatum neque cupiditate non! Unde hic vitae harum nisi quia facere saepe soluta iure quam laudantium pariatur totam similique magnam fugit, illum ullam natus.
         </p>
+        <a className="post-link" href="https://youtu.be/aZfrflVacaI" target="_blank" rel="noreferrer"><i className="fab fa-youtube post-link_icon"></i>Video del sitio</a>
       
     </div>
     )
