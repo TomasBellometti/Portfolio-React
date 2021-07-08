bada6906303561553e5b5fe4cdc3e99d4a743c48
TomasBellometti <tomasbellometti@gmail.com>
2021-07-08 16:07:34 -0300

    agregado de redes al footer

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -3,7 +3,14 @@ import './footer.css'
 function Footer() {
     return (
         <div className="footer">
-            Footer
+            <div className="sidebar-item">
+                <ul className="sidebar-item__redes">
+                    <li><i className="fab fa-linkedin sidebar-item__icons"></i></li>
+                    <li><i className="fab fa-facebook-square sidebar-item__icons"></i></li>
+                    <li><i className="fab fa-instagram-square sidebar-item__icons"></i></li>
+                    <li><i className="fab fa-git-square sidebar-item__icons"></i></li>
+                </ul>               
+           </div>
         </div>
     )
 }
